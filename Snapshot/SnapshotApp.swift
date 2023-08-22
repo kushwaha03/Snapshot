eb6c771ad3f1be99a508204b3938be5329ddfe14
radhakrishan <radhakrishan.r@ril.com>
2023-08-22 13:50:45 +0530

    Initial Commit

---
+++ Snapshot/SnapshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnapshotApp.swift
+//  Snapshot
+//
+//  Created by Mobile Programming  on 22/08/23.
+//
+
+import SwiftUI
+
+@main
+struct SnapshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
